117cf16a24e522683a3ae089476af4c9f8e3f39a
vpe0001 <vpe0001@alu.ubu.es>
2015-02-26 19:27:16 +0100

    Añadido test releaseReusable

--- src/ubu/gii/dass/c01/test/PoolObjectTest.java
+++ src/ubu/gii/dass/c01/test/PoolObjectTest.java
@@ -2,9 +2,11 @@ package ubu.gii.dass.c01.test;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 import ubu.gii.dass.c01.NotFreeInstanceException;
+import ubu.gii.dass.c01.Reusable;
 import ubu.gii.dass.c01.ReusablePool;
 
 public class PoolObjectTest {
@@ -21,6 +23,20 @@ public class PoolObjectTest {
 		pool.acquireReusable();
 		pool.acquireReusable();
 		pool.acquireReusable();
+	}
+	
+	@Test
+	public void testReleaseReusable() throws NotFreeInstanceException{
+		ReusablePool pool = ReusablePool.getInstance();
+		
+		Reusable reusable1 = pool.acquireReusable();
+		Reusable reusable2 = pool.acquireReusable();
+		
+		pool.releaseReusable(reusable2);
+		
+		Reusable reusable3 = pool.acquireReusable();
+		
+		assertEquals(reusable2, reusable3);
 	}
 
 }
