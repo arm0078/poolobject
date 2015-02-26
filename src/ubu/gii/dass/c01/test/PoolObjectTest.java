342032a3df573af11a3365ed8c634c2a137759ba
arm0078 <arm0078@alu.ubu.es>
2015-02-26 19:04:58 +0100

    Añadido test excepcion

--- src/ubu/gii/dass/c01/test/PoolObjectTest.java
+++ src/ubu/gii/dass/c01/test/PoolObjectTest.java
@@ -4,14 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import ubu.gii.dass.c01.NotFreeInstanceException;
 import ubu.gii.dass.c01.ReusablePool;
 
 public class PoolObjectTest {
-	
+
 	@Test
-	public void testAdquirirPool(){
+	public void testAdquirirPool() {
 		ReusablePool pool = ReusablePool.getInstance();
 		assertNotNull(pool);
+	}
+
+	@Test(expected = NotFreeInstanceException.class)
+	public void testAcquireReusableException() throws NotFreeInstanceException{
+		ReusablePool pool = ReusablePool.getInstance();
+		pool.acquireReusable();
+		pool.acquireReusable();
+		pool.acquireReusable();
 	}
 
 }
